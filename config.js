1465252e854a8ef8c9cefc4e0033ba29870d94c4
Xuzz <167371857+xuzzcode@users.noreply.github.com>
2024-12-23 23:14:20 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    token: "", 
-    prefix: ".",
+    token: "MTMyMDg0NDc2NjY1Njg1NjA3NA.GY-8LH.aTaWX3k2MwuiMlnMcbEQLXZ9bFsRcCCyxW3lvA", 
+    prefix: "?",
     destek: "",
     topgg: "",
-    davet: "",
+    davet: "https://discord.com/oauth2/authorize?client_id=1320844766656856074&scope=bot%20applications.commands&permissions=8",
     website: ""
 }
 
